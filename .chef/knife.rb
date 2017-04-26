c31db380e32792e8bad99c979afe4984ebe6bc4d
enbarrygit <enbarry@yahoo.com>
2017-04-25 23:08:40 -0400

    Update knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -8,7 +8,7 @@ client_key               "#{current_dir}/enbarry.pem"
 chef_server_url          "https://api.chef.io/organizations/ebridge"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 # AWS
-knife[:aws_access_key_id] = "AKIAIE2U5SPT42NAYCIA"
-knife[:aws_secret_access_key] = "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"
+knife[:aws_access_key_id] = ""
+knife[:aws_secret_access_key] = ""
 knife[:ssh_key_name] = "…"
 knife[:region] = "us-east-1"
